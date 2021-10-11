169d99665877da5a82987154acaf9f27ac5d605b
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-11 13:35:48 -0700

    Changed schema to todos and verified unauthorized GET

---
+++ data/todos.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    // id: 1,
+    todo : 'Wash dishes',
+    completed: false,
+    user_id: 1
+  },
+  {
+    // id: 2,
+    todo : 'Mow lawn',
+    completed: false,
+    user_id: 1
+  },
+  {
+    // id: 3,
+    todo : 'Wrangle children',
+    completed: false,
+    user_id: 1
+  },
+  
+];
